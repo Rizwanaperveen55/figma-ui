bf7dc897ead8afdb49bc5a7e4dfc8674a7235b7a
muhammad ahmed noorani <ahmednoorani258@gmail.com>
2024-12-08 18:55:37 -0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-import Image from "next/image";
-
+import HeroSection from "@/components/HeroSection";
+import HeroTwo from "@/components/Herotwo";
+import ShortSec from "@/components/ShortSec";
+import NewArrivals from "@/components/NewArrivals";
+import BlogSection from "@/components/BlogSection"
+import Instasec from "@/components/Instasec"
 export default function Home() {
   return (
-    <div>hello uiux hackathon coming</div>
+    <div>
+      <HeroSection />
+      <HeroTwo />
+      <ShortSec
+        title="Top Picks For You"
+        description="find a bright ideal to suit your taste with our great selection of suspension, floor and table lights"
+      />
+      <NewArrivals/>
+      <BlogSection/>
+      <Instasec/>
+    </div>
   );
 }
